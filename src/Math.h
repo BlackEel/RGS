1209e7946f36a2176bd29d91652a0a2007bdc614
BlackEel <3513924511@qq.com>
2025-07-09 20:54:54 +0800

    4 矩阵变换

--- src/Math.h
+++ src/Math.h
@@ -1,17 +1,103 @@
 #pragma once
+#include <string>
 
 namespace RGS {
+	struct Vec2
+	{
+		float X, Y;
+
+		constexpr Vec2()
+			: X(0.0f), Y(0.0f){}
+		constexpr Vec2(float x, float y)
+			: X(x), Y(y){}
+	};
+
 	struct Vec3
 	{
-		float X;
-		float Y;
-		float Z;
+		float X, Y, Z;
 
 		constexpr Vec3()
 			: X(0.0f), Y(0.0f), Z(0.0f){}
 		constexpr Vec3(float x, float y, float z)
 			: X(x), Y(y), Z(z){}
+
+		operator Vec2() const { return { X, Y }; }
 	};
+
+	struct Vec4
+	{
+		float X, Y, Z, W;
+
+		constexpr Vec4()
+			: X(0.0f), Y(0.0f), Z(0.0f), W(0.0f) {}
+		constexpr Vec4(float x, float y, float z, float w)
+			: X(x), Y(y), Z(z), W(w){}
+		constexpr Vec4(const Vec3& vec3, float w)
+			: X(vec3.X), Y(vec3.Y), Z(vec3.Z), W(w){}
+
+		operator Vec2() const { return { X,Y }; }
+		operator Vec3() const { return { X,Y,Z }; }
+
+		operator std::string() const
+		{
+			std::string res;
+			res += "(";
+			res += std::to_string(X);
+			res += ",";
+			res += std::to_string(Y);
+			res += ",";
+			res += std::to_string(Z);
+			res += ",";
+			res += std::to_string(W);
+			res += ")";
+			return res;
+		}
+
+	};
+
+	struct Mat4
+	{
+		float M[4][4];
+
+		Mat4()
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					M[i][j] = 0.0f;
+				}
+			}
+		}
+
+		Mat4(const Vec4& v0, const Vec4& v1, const Vec4& v2, const Vec4& v3);
+
+		operator const std::string() const
+		{
+			std::string res;
+			res += "(";
+			for (int i = 0; i < 4; i++)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					res += std::to_string(M[i][j]);
+					res += (i == 3 && j == 3) ? ")" : ",";
+				}
+				return res;
+			}
+		}
+	};
+
+	Vec4 operator* (const Mat4& mat4, const Vec4& vec4);
+	Mat4 operator* (const Mat4& left, const Mat4& right);
+	Mat4& operator*= (Mat4& left, const Mat4& right);
+
+	Mat4 Mat4Identity();
+	Mat4 Mat4Scale(float sx, float sy, float sz);
+	Mat4 Mat4Translate(float tx, float ty, float tz);
+	Mat4 Mat4RotateX(float angle);
+	Mat4 Mat4RotateY(float angle);
+	Mat4 Mat4RotateZ(float angle);
 
 	unsigned char Float2UChar(const float f);
 	float UChar2Float(const unsigned char c);
